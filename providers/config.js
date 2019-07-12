c87cd560fb082c1964798b417b1a6e2cf3da7a06
nicole.fu <nicole.fu@nidone.com.cn>
2019-07-12 14:06:57 +0800

    第一版本提交

--- providers/config.js
+++ providers/config.js
@@ -1,3 +1,4 @@
 axios.defaults.baseURL = 'http://h5.mind-node.com/';
+// axios.defaults.baseURL = 'http://10.5.192.67:8888/';
 // axios.defaults.headers.common['Authorization'] = null;
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
